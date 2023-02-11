7b24bf677faf65761d8d0033d82cb561f6b73042
Mathieu <mathieu.ravaut@gmail.com>
2023-02-11 16:47:03 +0800

    params for demo

--- src/summafusion/demo.py
+++ src/summafusion/demo.py
@@ -13,6 +13,26 @@ from engine import generation_step
 from generation_utils import GenerationMixin
 
 
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+args.device = torch.device("cuda")
+args.use_source = True
+args.use_candidates = True
+args.classify_candidates = True
+args.hidden_size = 1024
+args.use_source_for_cls = True
+args.cls_hidden_size = 2048
+args.scoring_methods = ["rouge_1", "rouge_2", "rouge_l"]
+args.max_candidate_length = 34
+args.n_candidates_to_use = 15
+args.max_source_length = 1024
+args.num_gen_beams = 10
+args.num_return_sequences = 1
+args.max_gen_summary_length = 64
+args.repetition_penalty = 1.0
+args.length_penalty = 0.8
+args.no_repeat_ngram_size = 3
+
 # seed
 seed_everything(42)
 
@@ -66,22 +86,21 @@ model = model.cuda()
 summafusion_model = ModelAbstractiveFusion(model, tokenizer, None)
 summafusion_model = summafusion_model.cuda()
 # prepare the data
-max_candidate_length = 34
 candidates_inputs, candidates_masks = [], []
 for j in range(len(candidates)):
     candidate = candidates[j]
     candidate = "CAND_{} ".format(j) + candidate
-    candidate_inputs = tokenizer(candidate, return_tensors="pt", truncation=True, padding="max_length", max_length=max_candidate_length)
-    candidate_inputs["input_ids"] = candidate_inputs["input_ids"][:, :max_candidate_length]
-    candidate_inputs["attention_mask"] = candidate_inputs["attention_mask"][:, :max_candidate_length]
+    candidate_inputs = tokenizer(candidate, return_tensors="pt", truncation=True, padding="max_length", max_length=args.max_candidate_length)
+    candidate_inputs["input_ids"] = candidate_inputs["input_ids"][:, :args.max_candidate_length]
+    candidate_inputs["attention_mask"] = candidate_inputs["attention_mask"][:, :args.max_candidate_length]
     candidates_inputs.append(candidate_inputs["input_ids"][0])
     candidates_masks.append(candidate_inputs["attention_mask"][0])
 candidates_inputs = torch.cat(candidates_inputs)
 candidates_masks = torch.cat(candidates_masks)
 print(candidates_inputs.shape, candidates_masks.shape)
-source_inputs = tokenizer(text, return_tensors="pt", truncation=True, padding="max_length", max_length=1024)
-source_ids = source_inputs["input_ids"][:, :1024]
-source_mask = source_inputs["attention_mask"][:, :1024]
+source_inputs = tokenizer(text, return_tensors="pt", truncation=True, padding="max_length", max_length=args.max_source_length)
+source_ids = source_inputs["input_ids"][:, :args.max_source_length]
+source_mask = source_inputs["attention_mask"][:, :args.max_source_length]
 batch = {
         "cand_ids": candidates_inputs.unsqueeze(0),
         "cand_mask": candidates_masks.unsqueeze(0),
@@ -90,18 +109,6 @@ batch = {
 }
 # inference
 gm = GenerationMixin
-parser = argparse.ArgumentParser()
-args = parser.parse_args()
-args.device = torch.device("cuda")
-args.max_source_length = 1024
-args.use_source = True
-args.use_candidates = True
-args.num_gen_beams = 10
-args.num_return_sequences = 1
-args.max_gen_summary_length = 64
-args.repetition_penalty = 1.0
-args.length_penalty = 0.8
-args.no_repeat_ngram_size = 3
 generated = generation_step(batch, tokenizer, summafusion_model, gm, args)
 print(generated)
 
